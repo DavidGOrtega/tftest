d9daff439e0921ba3b39a080f38534cc50985edd
DavidGOrtega <g.ortega.david@gmail.com>
2018-02-15 03:22:58 +0100

    sandalone

--- src/android/src/com/neocolorapp/tensorflow/TFBridgeCordova.java
+++ src/android/src/com/neocolorapp/tensorflow/TFBridgeCordova.java
@@ -21,158 +21,123 @@ import java.io.PrintWriter;
 import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
 public class TFBridgeCordova extends CordovaPlugin 
-{
-    
-    private static final int NUM_STYLES     = 26;
-                private static final String INPUT_NODE  = "input";
-                private static final String STYLE_NODE  = "style_num";
-                private static final String OUTPUT_NODE = "transformer/expand/conv3/conv/Sigmoid";
-                
+{      
     private TensorFlowInferenceInterface tfii;
 
     @Override
     public boolean execute(final String action, final JSONArray args, final CallbackContext callbackContext) throws JSONException 
     {
-        if ( action.equals("load") || action.equals("stylize") || action.equals("stylize_64") || action.equals("throw") || action.equals("throw2")  ) 
+        if ( action.equals("load") || action.equals("stylize")  ) 
         {
-                    
-        cordova.getThreadPool().execute(new Runnable()
-        {
-            public void run() 
+            cordova.getThreadPool().execute(new Runnable()
             {
-                if ( action.equals("load") ) 
+                public void run() 
                 {
                     try
                     {
-                        load( args.getString(0) );
-                        callbackContext.success("Model loaded successfully");
+                        if ( action.equals("load") ) 
+                        {
+                            load( args.getString(0) );
+                            callbackContext.success("Model loaded successfully");
                         
-                    }catch(Exception e)
-                    {
-                        callbackContext.error( e.getMessage() );
-                    }
-                
-                }else if ( action.equals("stylize") ) 
-                {
-                    try
-                    {
-                        String dat          = args.getString(0);
-                        JSONArray image_arr = new JSONArray(dat); //args.getJSONArray(0);
-                        int img_width       = args.getInt(1);
-                        int img_height      = args.getInt(2);
-                        
-                        float[] img_data    = new float[ image_arr.length() ]; 
-                        for (int i = 0; i < image_arr.length(); i++)
-                            img_data[i] = (float) image_arr.getDouble(i);
-
-                       /*  JSONArray style_arr = args.getJSONArray(3);
-                        float[] styles      = new float[ style_arr.length() ];   
-                        for (int i = 0; i < style_arr.length(); i++)
-                            styles[i] = (float) style_arr.getDouble(i);
+                        }else if ( action.equals("stylize_deprecated") ) 
+                        {
+                            String dat          = args.getString(0);
+                            JSONArray image_arr = new JSONArray(dat); //args.getJSONArray(0);
+                            int img_width       = args.getInt(1);
+                            int img_height      = args.getInt(2);
                             
-                        String[] logs       = null; */
-                        
-                        float[] styles   = new float[NUM_STYLES];
-                        for (int i = 0; i < NUM_STYLES; ++i) 
-                        {
-                            styles[i] = 1.0f / NUM_STYLES;
+                            float[] img_data    = new float[ image_arr.length() ]; 
+                            for (int i = 0; i < image_arr.length(); i++)
+                                img_data[i] = (float) image_arr.getDouble(i);
+
+                           /*  JSONArray style_arr = args.getJSONArray(3);
+                            float[] styles      = new float[ style_arr.length() ];   
+                            for (int i = 0; i < style_arr.length(); i++)
+                                styles[i] = (float) style_arr.getDouble(i);
+                                
+                            String[] logs       = null; */
+                            
+                            float[] styles   = new float[NUM_STYLES];
+                            for (int i = 0; i < NUM_STYLES; ++i) 
+                            {
+                                styles[i] = 1.0f / NUM_STYLES;
+                            }
+
+                            
+                            //stylize(img_data, img_width, img_height, styles, logs);
+
+                            tfii.feed( "input", img_data, 1, img_width, img_height, 3 );
+                            tfii.feed( "style_num", styles, styles.length );
+                            tfii.run( new String[]{"transformer/expand/conv3/conv/Sigmoid"}, true );              // Execute the output node's dependency sub-graph.
+                            tfii.fetch("transformer/expand/conv3/conv/Sigmoid", img_data);                        // Copy the data from TensorFlow back into our array.
+                            
+                            JSONObject output   = new JSONObject();
+                            output.put("success", true);
+                            //output.put("styles", Arrays.toString(styles) );
+                            //output.put("result1", Arrays.toString(img_data) );
+                            //output.put("logs", logs);
+                            
+                            callbackContext.success( output );
+
+                        //https://arxiv.org/abs/1610.07629 A Learned Representation For Artistic Style
+                        }else if ( action.equals("stylize") ) 
+                        {       
+                            final String INPUT_NODE  = "input";
+                            final String STYLE_NODE  = "style_num";
+                            final String OUTPUT_NODE = "transformer/expand/conv3/conv/Sigmoid";
+                            
+                            final String model  = args.getString(0);
+                            final String input  = args.getString(1);
+                            JSONArray styles_   = args.getJSONArray(2);
+
+                            byte[] bytes_in     = Base64.decode(input, Base64.DEFAULT);
+                            Bitmap bitmap       = BitmapFactory.decodeByteArray(bytes_in, 0, bytes_in.length); 
+                            Bitmap bitmap2      = bitmap.copy( bitmap.getConfig(), true );
+
+                            int[] intValues     = new int[ bitmap.getWidth() * bitmap.getHeight() ];
+                            bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+                            float[] floatValues = new float[ bitmap.getWidth() * bitmap.getHeight() * 3 ];
+                            for (int i = 0; i < intValues.length; ++i) 
+                            {
+                                final int val           = intValues[i];
+                                floatValues[i * 3]      = ((val >> 16) & 0xFF) / 255.0f;
+                                floatValues[i * 3 + 1]  = ((val >> 8) & 0xFF) / 255.0f;
+                                floatValues[i * 3 + 2]  = (val & 0xFF) / 255.0f;
+                            }
+
+                            float[] styles      = new float[ styles_.length() ];   
+                            for (int i = 0; i < styles_.length(); i++)
+                                styles[i] = (float) styles_.getDouble(i);
+
+                            TensorFlowInferenceInterface tfii = new TensorFlowInferenceInterface( this.cordova.getActivity().getAssets(), model );
+                            tfii.feed(INPUT_NODE, floatValues, 1, bitmap.getWidth(), bitmap.getHeight(), 3);
+                            tfii.feed(STYLE_NODE, styles, styles.length);
+                            tfii.run(new String[] {OUTPUT_NODE}, true);
+                            tfii.fetch(OUTPUT_NODE, floatValues);
+
+                            for (int i = 0; i < intValues.length; ++i) 
+                            {
+                                intValues[i] =
+                                    0xFF000000
+                                    | (((int) (floatValues[i * 3] * 255)) << 16)
+                                    | (((int) (floatValues[i * 3 + 1] * 255)) << 8)
+                                    | ((int) (floatValues[i * 3 + 2] * 255));
+                            }
+
+                            bitmap2.setPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+                            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();  
+                            bitmap2.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+                            byte[] byteArray    = byteArrayOutputStream .toByteArray();
+                            String base64_out   = Base64.encodeToString(byteArray, Base64.DEFAULT);
+
+                            JSONObject output   = new JSONObject();
+                            output.put("out", base64_out);
+                            callbackContext.success( output );
                         }
-
-                        
-                        //stylize(img_data, img_width, img_height, styles, logs);
-
-                        tfii.feed( "input", img_data, 1, img_width, img_height, 3 );
-                        tfii.feed( "style_num", styles, styles.length );
-                        tfii.run( new String[]{"transformer/expand/conv3/conv/Sigmoid"}, true );              // Execute the output node's dependency sub-graph.
-                        tfii.fetch("transformer/expand/conv3/conv/Sigmoid", img_data);                        // Copy the data from TensorFlow back into our array.
-                        
-                        JSONObject output   = new JSONObject();
-                        output.put("success", true);
-                        //output.put("styles", Arrays.toString(styles) );
-                        //output.put("result1", Arrays.toString(img_data) );
-                        //output.put("logs", logs);
-                        
-                        callbackContext.success( output );
-                        
-                    }catch(Exception e)
-                    {
-                        callbackContext.error( e.getMessage() );
-                    }
-                
-                }else if ( action.equals("stylize_64") ) 
-                {       
-                    try
-                    {
-                        float[] styleVals   = new float[NUM_STYLES];
-                        for (int i = 0; i < NUM_STYLES; ++i) 
-                        {
-                            styleVals[i] = 1.0f / NUM_STYLES;
-                        }
-
-                        byte[] bytes_in     = Base64.decode(args.getString(0), Base64.DEFAULT);
-                        Bitmap bitmap       = BitmapFactory.decodeByteArray(bytes_in, 0, bytes_in.length); 
-
-                        float[] floatValues = new float[ bitmap.getWidth() * bitmap.getHeight() * 3 ];
-                        int[] intValues     = new int[ bitmap.getWidth() * bitmap.getHeight() ];
-
-                        Bitmap bitmap2      = bitmap.copy( bitmap.getConfig(), true );
-                        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
-
-                        for (int i = 0; i < intValues.length; ++i) 
-                        {
-                            final int val           = intValues[i];
-                            floatValues[i * 3]      = ((val >> 16) & 0xFF) / 255.0f;
-                            floatValues[i * 3 + 1]  = ((val >> 8) & 0xFF) / 255.0f;
-                            floatValues[i * 3 + 2]  = (val & 0xFF) / 255.0f;
-                        }
-
-                        tfii.feed(INPUT_NODE, floatValues, 1, bitmap.getWidth(), bitmap.getHeight(), 3);
-                        tfii.feed(STYLE_NODE, styleVals, NUM_STYLES);
-                        tfii.run(new String[] {OUTPUT_NODE}, true);
-                        tfii.fetch(OUTPUT_NODE, floatValues);
-
-                        for (int i = 0; i < intValues.length; ++i) 
-                        {
-                          intValues[i] =
-                              0xFF000000
-                                  | (((int) (floatValues[i * 3] * 255)) << 16)
-                                  | (((int) (floatValues[i * 3 + 1] * 255)) << 8)
-                                  | ((int) (floatValues[i * 3 + 2] * 255));
-                        }
-
-                        bitmap2.setPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
-
-                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();  
-                        bitmap2.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
-                        byte[] byteArray    = byteArrayOutputStream .toByteArray();
-                        String base64_out   = Base64.encodeToString(byteArray, Base64.DEFAULT);
-
-                        JSONObject output   = new JSONObject();
-                        output.put("out", base64_out);
-                        callbackContext.success( output );
-
-                    }catch(Exception e)
-                    {
-                        StringWriter errors = new StringWriter();
-                        e.printStackTrace( new PrintWriter(errors) );
-                        callbackContext.error( errors.toString() );
-                    }
-                
-                }else if ( action.equals("throw") ) 
-                {
-                    float number1 = 1;
-                    float number2 = 0;
                     
-                    float number3 = number1 / number2;
-                    
-                    int[] jj = new int[3];
-                    jj[4] = 23;
-                
-                }else if ( action.equals("throw2") ) 
-                {
-                    try
-                    {
-                        throw new JSONException("mother fucker");
                     }catch(Exception e)
                     {
                         StringWriter errors = new StringWriter();
@@ -180,8 +145,7 @@ public class TFBridgeCordova extends CordovaPlugin
                         callbackContext.error( errors.toString() );
                     }
                 }
-            }
-        });
+            });
         
             return true;
         }
